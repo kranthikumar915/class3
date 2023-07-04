16b83ece02e2238e765dae6786eaad92412bcb8b
kranthikumar915 <rcbkranthikumar@gmail.com>
2023-07-04 11:59:24 +0530

    react calculator

--- src/component/Calculator.jsx
+++ src/component/Calculator.jsx
@@ -3,6 +3,8 @@ import Button from './sub/Button'
 import Screen from './sub/Screen'
 import './calc.css'
 
+/* eslint no-eval: 0 */
+
 
 function Calculator(props) {
     const [inp,setInp] = useState('0')
